6936b6b50130b153f169d8eeb887c81d05d36d6c
addirossi <addirossi@gmail.com>
2022-11-23 00:10:16 +0600

    Add test

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,2 +1,8 @@
-def test_dummy():
-    assert True
+from rest_framework.test import APIClient, APITestCase
+
+
+class TestAPIViews(APITestCase):
+    def test_sample_view(self):
+        client = APIClient()
+        response = client.get('/test/')
+        self.assertEqual(response.status_code, 200)
